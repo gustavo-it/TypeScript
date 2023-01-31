b3b8573e4e927fbbc0e94a69a40e3f2eb26db139
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-01-31 10:46:43 -0300

    Tipos Literais

---
+++ 12.tipos_litereais/index.ts
@@ -0,0 +1,8 @@
+// Imagine que você tem uma constante y com o valor de 10. 
+// Essa constante é um valor literal, pois para sempre o seu valor será 10, não podendo ser alterado.
+
+function escolhaCor(cor: 'vermelho' | 'amarelo' | 'azul'): string {
+    return cor;
+}
+// O exemplo acima também são valores literais
+// O valor da cor não pode ser diferente de vermelho, amarelo, azul
